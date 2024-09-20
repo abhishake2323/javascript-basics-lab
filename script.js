105a590be4f8dfeab87cbc775a4dc7fab5e70280
abhishake2323 <abhishakem@hotmail.com>
2024-09-20 18:49:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// Example: Declaring and using a variable
+let greeting = "Hello, World!";
+console.log(greeting); // This should print "Hello, World!" in the console
+
+let studentName = "Abhishek";
+let studentAge  = 34;
+let isEnrolled  = true;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+
+//five data types in javascript - 	Demonstrate Data Types
+
+
+let stringExample ="This is a sample string";
+let numberExample =2024;
+let floatingNumberExample = 95.5;
+let boolExample =false;
+let arrayExample     =["honda","toyota","tesla","ford","bmw"];
+let objectExample ={id:1002, firstName: "Eric",lastname: "David"};
+
+console.log("String :",stringExample)
+console.log("Number :",numberExample)
+console.log("Floating number :",floatingNumberExample)
+console.log("boolean :",boolExample)
+console.log("array :",arrayExample)
+console.log("Object :",objectExample)
+
+ 
+
+// 1.	Use Basic Operators
+
+// •	Perform and log operations using different types of operators:
+// •	Arithmetic Operators: Addition (+), Subtraction (-), Multiplication (*), and Division (/).
+
+let num1 = 20 
+let num2 = 10
+
+console.log(`Addition of ${num1} , ${num2} :`, num1+num2)
+console.log(`Subtraction of ${num1} , ${num2} :`, num1-num2)
+console.log(`Multiplication of ${num1} , ${num2} :`, num1*num2)
+console.log(`Division of  ${num1} , ${num2} :`, num1/num2)
+
+
+// •	Comparison Operators: Greater than (>), Less than (<), Equal to (===), and Not equal (!==).
+
+console.log(`Is  ${num1} greater than ${num2} :`, num1>num2)
+console.log(`Is  ${num1} less than  ${num2} :`, num1<num2)
+console.log(`Is ${num1} equal to ${num2} :`, num1===num2)
+console.log(`Is ${num1} Not equal to ${num2} :`, num1!==num2)
+
+// •	Logical Operators: AND (&&), OR (||), and NOT (!).
+let bool1 = true
+let bool2 = false
+console.log(`Logical AND ( && ) between ${bool1} and ${bool2} is `, bool1&&bool2)
+console.log(`Logical OR ( || ) between ${bool1} and ${bool2} is `, bool1||bool2)
+console.log(`Logical NOT (! ) on ${bool1} is `, !bool1)
+console.log(`Logical NOT (! ) on ${bool2} is `, !bool2)
+
+
